138be53251de96ad9f398c88e554e8427d26861e
Maia Federica_G <maiafedericag@gmail.com>
2024-02-19 14:35:35 -0300

    Mi codigo exploto y estoy tratando de hallar el problema

--- src/components/CardNoticias.jsx
+++ src/components/CardNoticias.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
-const CardNoticias = ({noticiaProps}) => {
-    return (
-        <>
-            <div className="card" style="width: 18rem;">
-  <div className="card-body">
-    <h5 className="card-title">{noticiaProps.title}</h5>
-    <p className="card-text">{noticiaProps.description}</p>
-    <a href={noticiaProps.url} className="btn btn-primary">Más infromacion</a>
-  </div>
-</div>
-</>
-    );
+const CardNoticias = ({ noticiaProps }) => {
+ 
+  return (
+    <div className="card">
+      <img src={noticiaProps.urlToImage} alt={noticiaProps.title} />
+      <h2>{noticiaProps.title}</h2>
+      <p>{noticiaProps.description}</p>
+      <a href={noticiaProps.url} className='btn btn-primary'>Mas informacion</a>
+    </div>
+  );
 };
 
 export default CardNoticias;
+
