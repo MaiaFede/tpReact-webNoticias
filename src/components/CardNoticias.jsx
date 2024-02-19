9f4c92cac6c5ab38e712a5bb3e980b5180ba5027
Maia Federica_G <maiafedericag@gmail.com>
2024-02-19 14:00:13 -0300

    puse los props de noticia en la card

--- src/components/CardNoticias.jsx
+++ src/components/CardNoticias.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const CardNoticias = () => {
+const CardNoticias = ({noticiaProps}) => {
     return (
         <>
             <div className="card" style="width: 18rem;">
-  <img className="card" style="width: 18rem;" />
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{noticiaProps.title}</h5>
+    <p className="card-text">{noticiaProps.description}</p>
+    <a href={noticiaProps.url} className="btn btn-primary">Más infromacion</a>
   </div>
 </div>
 </>
